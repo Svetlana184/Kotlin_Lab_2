c662ccfa0e3aa9003118e613db66f11f08ae2548
Svetlana184 <sveta.kgd.39@gmail.com>
2025-10-11 11:21:35 +0200

    lab 3 1-2

--- src/LAB_3.kt
+++ src/LAB_3.kt
@@ -1,7 +1,12 @@
+import java.time.LocalDate
+
 fun main() {
     var tom = Employee("Ivanov", 100000, 2015)
     println(tom.DaysExpirience())
     println(tom.Experience())
+
+    var alice = DopEmployee("Alice", 100000, 2015, 1998)
+    alice.yearsToWork()
 }
 
 open class Employee{
@@ -17,19 +22,19 @@ open class Employee{
         get() = field
     private var salary:Int = 1
         set(value) {
-            if((value > 0) and (value < 100)) field = value
+            if(value > 0) field = value
         }
         get() = field
-    private var year:Int = 1
+    private var year:Int = LocalDate.now().year
         set(value) {
-            if((value > 0) and (value < 100)) field = value
+            if((value > 0) and (value <= LocalDate.now().year)) field = value
         }
         get() = field
     fun Experience():Int{
-        return  2025-year
+        return  LocalDate.now().year-year
     }
     fun DaysExpirience():Int{
-        return (2025-year)*365
+        return (LocalDate.now().year-year)*365
     }
 }
 class DopEmployee(surname: String, salary: Int, year: Int, yearOfBirth: Int) : Employee(surname, salary, year) {
