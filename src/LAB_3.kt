77375dded29c4ef0f955e09cd8ddc4e6a86ca408
Svetlana184 <sveta.kgd.39@gmail.com>
2025-10-11 11:35:10 +0200

    lab 3 3

--- src/LAB_3.kt
+++ src/LAB_3.kt
@@ -7,6 +7,11 @@ fun main() {
 
     var alice = DopEmployee("Alice", 100000, 2015, 1998)
     alice.yearsToWork()
+
+    var firstCar = Car("BMW", 230.0, 3)
+    println(firstCar.Quantity())
+    var secondCar = SuperCar("Michael", 230.0, 3, 2000)
+    println(secondCar.Quantity())
 }
 
 open class Employee{
@@ -38,6 +43,7 @@ open class Employee{
     }
 }
 class DopEmployee(surname: String, salary: Int, year: Int, yearOfBirth: Int) : Employee(surname, salary, year) {
+
     private var yearOfBirth:Int = 18
         set(value) {
             if(value > 0) field = value
@@ -46,5 +52,42 @@ class DopEmployee(surname: String, salary: Int, year: Int, yearOfBirth: Int) : E
     fun yearsToWork(){
         if(yearOfBirth>60) println("${yearOfBirth - 60} лет работает после 60")
         else println("${60 - yearOfBirth} лет осталось работать до 60")
+    }
+}
+
+open class Car{
+    constructor(mark:String, power:Double, countPlaces:Int) {
+        this.mark = mark
+        this.power = power
+        this.countPlaces = countPlaces
+    }
+    private var mark:String = "Noname"
+        set(value) {
+            field = value
+        }
+        get() = field
+    private var power:Double = 1.0
+        set(value) {
+            if(value > 0.0) field = value
+        }
+        get() = field
+    private var countPlaces:Int = 1
+        set(value) {
+            if(value > 0) field = value
+        }
+        get() = field
+    open fun Quantity():Double{
+        return 0.1 * countPlaces.toDouble() * power
+    }
+}
+
+class SuperCar(mark: String, power: Double, countPlaces: Int, year:Int) : Car(mark, power, countPlaces){
+    private var year:Int = 1
+        set(value) {
+            if(value > 0) field = value
+        }
+        get() = field
+    override fun Quantity():Double{
+        return super.Quantity() - 1.5 * (LocalDate.now().year - year)
     }
 }
