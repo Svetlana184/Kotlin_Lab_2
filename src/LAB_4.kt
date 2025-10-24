350d648b20dfb4730d544d3f361de7de39c7b110
Svetlana184 <sveta.kgd.39@gmail.com>
2025-10-24 11:18:25 +0200

    lab 4 almost done

--- src/LAB_4.kt
+++ src/LAB_4.kt
@@ -5,14 +5,40 @@
 //  - clear() - очистить контейнер
 //  - isEmpty(): Boolean - проверка на пустоту
 
-class Container<T>(var item:T){
+fun main() {
+    var item1 = Container<Int>(33)
+    item1.getItem()
+    item1.putItem(22)
+    item1.getItem()
+    println(item1.isEmpty())
+    item1.clearItem()
+    println(item1.isEmpty())
 
+    println()
+
+    var item2 = Container<String>("bob")
+    item2.getItem()
+    item2.putItem("alex")
+    item2.getItem()
+    println(item2.isEmpty())
+    item2.clearItem()
+    println(item2.isEmpty())
+}
+
+
+
+class Container<T>(var item:T){
+    fun putItem(item_:T){
+        item = item_
+    }
     fun getItem() {
         println(item)
     }
-//    fun clearItem(){
-//        item = <T>();
-//    }
-
-
+    fun clearItem(){
+        item = "" as T
+    }
+    fun isEmpty(): Boolean{
+        if(item == "" as T) return true
+        return false
+    }
 }
